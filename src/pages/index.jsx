3fb8c92bb2a4afba1070917235603920476f720e
Abhay Kumar <abhay.kumar@geminisolutions.in>
2020-10-07 01:41:38 +0530

    description added

--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -6,12 +6,8 @@ const First = () => {
       <br />
       <br />
       <p style={{ width: "40ch" }}>
-        Lorem ipsum dolor sit amet consectetur adipisicing elit. Cum odio harum
-        nulla. Magni magnam porro doloribus odit iure necessitatibus quibusdam
-        soluta reiciendis inventore? Illum modi, aspernatur voluptate
-        consequuntur nisi suscipit. Unde iste sunt asperiores necessitatibus
-        voluptate tenetur porro earum in laboriosam, nostrum provident magni
-        temporibus maiores aperiam id dolorem rem?
+        This is a sample next project with features like sass, fluid-layout, env
+        support, logger configuration, redux, modular scss and theme support
       </p>
     </div>
   );
