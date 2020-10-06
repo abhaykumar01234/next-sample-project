6ee141e1cc57368c89e4a3d8d0f3e642ac59552e
Abhay Kumar <abhay.kumar@geminisolutions.in>
2020-10-07 01:32:09 +0530

    first commit

--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -1,12 +1,10 @@
 const First = () => {
   return (
     <div>
-      <h1>My Idea Serious Men</h1>
-      <h2>My Idea Serious Men</h2>
-      <h3>My Idea Serious Men</h3>
-      <h4>My Idea Serious Men</h4>
-      <h5>My Idea Serious Men</h5>
-      <h6>My Idea Serious Men</h6>
+      <h1>My Website</h1>
+      <hr />
+      <br />
+      <br />
       <p style={{ width: "40ch" }}>
         Lorem ipsum dolor sit amet consectetur adipisicing elit. Cum odio harum
         nulla. Magni magnam porro doloribus odit iure necessitatibus quibusdam
